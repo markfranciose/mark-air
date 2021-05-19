e3b6db5461a29099d8b59fde7739d9122cde017a
Mark Franciose <mark.franciose@gmail.com>
2021-05-18 22:47:28 -0500

    create basic api dir && hooks

---
+++ src/api/api.ts
@@ -0,0 +1,36 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const AIRPLANE_URL = 'https://infinite-dawn-93085.herokuapp.com/aircrafts';
+const FLIGHT_URL = 'https://infinite-dawn-93085.herokuapp.com/flights';
+
+const ERROR_DATA = {
+  state: 'ERROR',
+  data: []
+};
+
+export function useFlights() { return useDataFetch(FLIGHT_URL); }
+export function useAirplanes() { return useDataFetch(AIRPLANE_URL); }
+
+function useDataFetch(url: string) {
+  const [data, setData] = useState({
+    state: 'LOADING',
+    data: []
+  });
+
+  useEffect(() => {
+    axios.get(url)
+      .then((result: any) => {
+        console.log(20, result.data.data);
+        setData({
+          state: 'DONE',
+          data: result.data.data
+        });
+      })
+      .catch(() => {
+        setData(ERROR_DATA);
+      });
+  }, []);
+
+  return data;
+}

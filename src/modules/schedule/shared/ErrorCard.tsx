045ddefe996b0c3faf495746e1d596ec7e8c92e7
Mark Franciose <mark.franciose@gmail.com>
2021-05-18 23:40:44 -0500

    add shared error card

--- src/modules/schedule/shared/ErrorCard.tsx
+++ src/modules/schedule/shared/ErrorCard.tsx
@@ -1,5 +1,20 @@
 import React from 'react';
+import { Card, Typography } from '@material-ui/core';
+import { Error } from '@material-ui/icons';
 
-export default function ErrorCard() {
-  return (<p>We error</p>);
+interface ErrorCardProps {
+  errorMessage: string;
+}
+
+export default function ErrorCard({
+  errorMessage
+}: ErrorCardProps) {
+  return (
+    <Card>
+      <Error />
+      <Typography>
+        { errorMessage }
+      </Typography>
+    </Card>
+  );
 }
